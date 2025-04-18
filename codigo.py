7c385267f54379dabb9d0745c219c2aadf7047bb
Gabrielle <134111687+GabsLadislau@users.noreply.github.com>
2025-04-18 18:08:01 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,6 +1,1 @@
-def calcular_total_vendas(vendas):
-    return sum(vendas)
-
-vendas = [100, 200, 300, 400, 500]
-total = calcular_total_vendas(vendas)
-print("Total de vendas:", total)
+vendas = [10, 20, 30]
